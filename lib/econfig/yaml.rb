c6d5d77d7c22b9aae9fe364dde8064600fda5b29
Jonas Nicklas <jonas.nicklas@gmail.com>
2013-01-07 21:50:10 +0100

    YAML backend doesn't die when file doesn't exist

--- lib/econfig/yaml.rb
+++ lib/econfig/yaml.rb
@@ -1,13 +1,21 @@
 module Econfig
   class YAML
+    def initialize(path="config/app.yml")
+      @path = path
+    end
+
     def get(key)
-      options[key]
+      options[key] if options
     end
 
   private
 
+    def path
+      File.expand_path(@path, Econfig.root)
+    end
+
     def options
-      @options ||= ::YAML.load_file(File.join(Econfig.root, "config/app.yml"))[Econfig.env]
+      @options ||= ::YAML.load_file(path)[Econfig.env] if File.exist?(path)
     end
   end
 end
--- spec/yaml_spec.rb
+++ spec/yaml_spec.rb
@@ -4,5 +4,14 @@ describe Econfig::YAML do
     it "fetches option from yaml config file" do
       backend.get("quox").should == "baz"
     end
+
+    it "returns nil when the option does not exist" do
+      backend.get("does_not_exist").should be_nil
+    end
+
+    it "returns nil when there is no config file" do
+      backend = Econfig::YAML.new("/does/not/exist")
+      backend.get("does_not_exist").should be_nil
+    end
   end
 end
