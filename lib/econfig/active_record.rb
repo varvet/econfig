820d3e544eb395d3a13303c1266da6a3ae6b25bb
Jonas Nicklas and Kim Burgestrand <dev+jonas+kim@elabs.se>
2015-10-13 09:05:23 +0200

    Add warning when ActiveRecord table doesn't exist

--- lib/econfig/active_record.rb
+++ lib/econfig/active_record.rb
@@ -1,5 +1,16 @@
+require "active_record"
+
 module Econfig
   class ActiveRecord
+    MISSING_TABLE_WARNING = <<-WARNING.gsub(/^ */, "")
+      =======================================================================
+      Econfig table not found. Please add the configuration table by running:
+
+      rails generate econfig:migration
+      rake db:migrate
+      =======================================================================
+    WARNING
+
     class Option < ::ActiveRecord::Base
       attr_accessible :key, :value rescue nil
       self.table_name = "econfig_options"
@@ -9,6 +20,9 @@ module Econfig
 
     def keys
       Set.new(Option.pluck(:key))
+    rescue ::ActiveRecord::StatementInvalid
+      warn MISSING_TABLE_WARNING
+      Set.new([])
     end
 
     def get(key)
@@ -17,11 +31,15 @@ module Econfig
       else
         yield if block_given?
       end
+    rescue ::ActiveRecord::StatementInvalid
+      warn MISSING_TABLE_WARNING
     end
 
     def set(key, value)
       option = Option.where(:key => key).first_or_initialize
       option.update_attributes!(:value => value)
+    rescue ::ActiveRecord::StatementInvalid
+      warn MISSING_TABLE_WARNING
     end
   end
 end
